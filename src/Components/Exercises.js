df04e966b289af37a16df18c1e778ff70bb9c3ce
JassiSingh08 <singhjassi693@gmail.com>
2023-04-12 11:38:28 +0530

    latest commits

--- src/Components/Exercises.js
+++ src/Components/Exercises.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
+import { Pagination} from "@mui/material";
+import { Box, Typography, Stack } from "@mui/material";
 
-const Exercises = () => {
+import { exerciseOptions, fetchData } from "../utils/fetchData";
+
+const Exercises = ({ exercises, setExercise, bodyPart }) => {
   return (
-    <div>Exercises</div>
-  )
-}
+    <Box id="exercises" sx={{ mt: { lg: "110px" } }} mt="50px" p="20px">
+      <Typography variant="h3" mb="46px">
+        Showing Results
+      </Typography>
+      <Stack
+        direction="row"
+        sx={{ gap: { lg: "110px", sx: "50px" } }}
+        flexWrap="wrap"
+        justifyContent="center"
+      >
+        {exercises.map((exercise, index) => (
+          <p>{exercise.name}</p>
+        ))}
+      </Stack>
+    </Box>
+  );
+};
 
 export default Exercises;
